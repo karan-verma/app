46eb89508d328a54e596649c0d2fefb06146e79c
karan-verma <karan.verma2012@vit.ac.in>
2015-03-30 19:33:17 +0530

    First Commit

---
+++ gen/com/theinvader360/racegame/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.theinvader360.racegame;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/theinvader360/racegame/AndroidStarter.java
@@ -0,0 +1,24 @@
+
+package com.theinvader360.racegame;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.theinvader360.racegame.RaceGame;
+
+public class AndroidStarter extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // prevent the screen from dimming/sleeping (no permission required)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new RaceGame(), cfg);
+    }
+}
